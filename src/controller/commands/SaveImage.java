cbebf389f7a4a8bd7e8b7b35a9559bf3cdce3e7f
jmarkowitz <markowitzjonathan@me.com>
2023-03-13 23:53:11 -0400

    Added other commands

---
+++ src/controller/commands/SaveImage.java
@@ -0,0 +1,14 @@
+package controller.commands;
+
+import controller.CollagerCommand;
+
+public class SaveImage implements CollagerCommand {
+
+  /**
+   * Method that will execute the command
+   */
+  @Override
+  public void execute() {
+
+  }
+}
---
+++ src/controller/commands/SetFilter.java
@@ -0,0 +1,14 @@
+package controller.commands;
+
+import controller.CollagerCommand;
+
+public class SetFilter implements CollagerCommand {
+//TODO: need to add functionality to support different types of filters? Interface?
+  /**
+   * Method that will execute the command
+   */
+  @Override
+  public void execute() {
+
+  }
+}
